49134091f4c2f5da3e92dd8103f52513b97780a5
Cody Price <dev.cprice@gmail.com>
2021-02-19 12:39:04 -0800

    try simple-git

--- pages/api/git.ts
+++ pages/api/git.ts
@@ -1,13 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { execSync } from "child_process";
+// import { execSync } from "child_process";
+import simpleGit from "simple-git";
 
-function gitInfo() {
-  return execSync("git rev-parse HEAD").toString().trim();
-}
+const git = simpleGit();
+
+// function gitInfo() {
+//   return execSync("git rev-parse HEAD").toString().trim();
+// }
 
 function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ hash: gitInfo() });
+  const hash = git.revparse("HEAD");
+
+  return res.status(200).json({ hash });
 }
 
 export default handler;
